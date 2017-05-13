bdbd057d1ae808801d85bb265594d0d695e5e23c
Bruno Martins de Carvalho <bruno_28_4@hotmail.com>
2017-05-13 11:44:54 -0300

    Parcial commit.
    Improvements on the interface.

--- CalculadoraCLT/app/src/main/java/com/brunocarvalho/calculadoraclt/fragments/SalLiquidoFragment.java
+++ CalculadoraCLT/app/src/main/java/com/brunocarvalho/calculadoraclt/fragments/SalLiquidoFragment.java
@@ -1,11 +1,13 @@
 package com.brunocarvalho.calculadoraclt.fragments;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.brunocarvalho.calculadoraclt.R;
 
@@ -14,6 +16,9 @@ import com.brunocarvalho.calculadoraclt.R;
  */
 
 public class SalLiquidoFragment extends Fragment {
+
+    private TextInputLayout inputLayoutSalBruto;
+    private EditText editTextSalBruto;
 
     public SalLiquidoFragment(){
 
@@ -28,5 +33,9 @@ public class SalLiquidoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_sal_liquido, container, false);
+    }
+
+    private boolean validateSalarioBruto(){
+        return false;
     }
 }
